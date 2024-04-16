bcd6ead136a57c90a4b792cf66c643c12752279f
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-04-16 13:46:36 -0500

    verceldb Server Component accessing a Postgres Database

--- src/conexion.py
+++ src/conexion.py
@@ -2,12 +2,11 @@
 import os
 import psycopg2
 
-print(os.getenv('POSTGRES_HOST'))
 connection = psycopg2.connect(
     host = os.getenv('POSTGRES_HOST'),
     user = os.getenv('POSTGRES_USER'),
-    password = os.environ.get('POSTGRES_PASSWORD'),
-    database = os.getenv('POSTGRES_DATABASE')
+    password = os.getenv('POSTGRES_PASSWORD'),
+    database = os.getenv('POSTGRES_DATABASE'),
+    port = 5432
 )
-
 print("Succefully connected")
