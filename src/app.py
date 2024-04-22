6f66934e7f6827bb4c023c20da453bd0cd941681
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-04-22 14:56:47 -0500

    get_taxis() function limited to 5 elements per page plus request module used

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@
 # Flask class. Instance of this class will be our WSGI application (Web Server Gateway Interface).
 import os
 from flask import Flask, jsonify
+from flask import request
 from flasgger import Swagger
 from models.taxi_model import db, Taxi
 
@@ -41,25 +42,46 @@ def get_taxis():
     """
     Get list of taxis
     ---
+    swagger: "3.0"
+    info:
+    description: "Documentation [Fleet Management API - Python](https://github.com/Wendy-Alejandra/DEV013-fleet-management-api/tree/main)"
+    version: "1.0.0-basic"
+    title: "Fleet management API"
+    tags:
+    - name: "taxis"
+        description: "Taxi operations"
+    paths:
+    /taxis:
+        get:
+        summary: "taxis list"
+        description: ""
+    parameters:
+    - name: limit
+    in: query
+    type: integer
+    description: Number of elements per page
+    default: 10
     responses:
-      200:
-        description: A list of taxis
+    200:
+        description: A list of taxis (successful operation)
         schema:
-          type: array
-          items:
+        type: array
+        items:
             $ref: '#/definitions/Taxi'
-      404:
+    404:
         description: No taxis found
-          properties:
+        properties:
             id:
-              type: integer
-              description: The taxi ID (primary key)
+            type: integer
+            description: The taxi ID (primary key)
             plate:
-              type: string
-              description: The taxi plate number (alphanumeric)
+            type: string
+            description: The taxi plate number (alphanumeric)
     """
-    taxis = Taxi.query.limit(10).all()
-    if not taxis:
+    # Limiting the number of elements per page
+    limit = int(request.args.get('limit', 5))
+    taxis = Taxi.query.limit(limit).all()
+    if not "/taxis":
         return jsonify({"error": "No taxis found"}), 404
     print(taxis)
     taxi_list = [{"id": taxi.id, "plate": taxi.plate} for taxi in taxis]
