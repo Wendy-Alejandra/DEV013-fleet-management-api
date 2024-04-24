55ab6d0b7eedab9d9152d8f2bc096ee0015a40d8
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-04-24 15:07:17 -0500

    endpoint /taxis pagination structure changed using request.args

--- src/app.py
+++ src/app.py
@@ -1,8 +1,6 @@
 """Flask application"""
-
-
 from flask import Flask, jsonify
-# from flask import request
+from flask import request
 from flasgger import Swagger
 from src.models.taxi_model import db, Taxi
 from src.config import Config
@@ -10,6 +8,8 @@ from src.config import Config
 # Instance of Flask class. Argument __name__ is the name of the application’s module or package.
 app = Flask(__name__)
 app.config.from_object(Config())
+
+# initialize the SQLAlchemy extension class with the application by calling :
 db.init_app(app)
 
 # Initialize Swagger
@@ -21,13 +21,16 @@ def index():
     return "Welcome to the Fleet Management API!"
 
 @app.route("/taxis", methods=["GET"])
-@app.route("/taxis/<int:page>", methods=["GET"])
+# @app.route("/taxis/<int:page>", methods=["GET"])
 def get_taxis(page=1):
     """Get list of taxis and pagination"""
+    # Filtering the data from db per page using request parameters
+    page = request.args.get('page', default=1, type=int)
+    per_page = request.args.get('per_page', default=10, type=int)
     # using Flask SQLAlchemy pagination object
     # As of Flask-SQLAlchemy 3.0, all arguments to paginate are keyword-only
     # .items on the current page list (as paginate() is an object so it is not iterable)
-    taxis = Taxi.query.paginate(page=page, per_page=10,error_out=False).items
+    taxis = Taxi.query.paginate(page=page, per_page=per_page,error_out=False).items
     print(taxis)
 
     # Build list of taxis
--- src/config.py
+++ src/config.py
@@ -6,13 +6,9 @@ from flask_sqlalchemy import SQLAlchemy
 # db from SQLAlchemy class imported from flask_sqlalchemy
 db = SQLAlchemy()
 class Config():
-    # Get URI connection to PostgreSQL from environment variables
+    """SQLAlchemy configuration"""
+    # Configure the SQLAlchemy database
     postgres_url = os.getenv('POSTGRES_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Get URI connection to PostgreSQL from environment variables
     SQLALCHEMY_DATABASE_URI = postgres_url
-    # Configure the SQLAlchemy database, relative to the app instance folder
-    # app.config["SQLALCHEMY_DATABASE_URI"] = postgres_url
-    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    # initialize the SQLAlchemy extension class with the application by calling :
-    # db.init_app(app)
