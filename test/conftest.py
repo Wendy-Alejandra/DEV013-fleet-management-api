982a61294ddd0db2a409f83cc15ab24c093b9a83
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-04-26 13:37:16 -0500

    Docstrings added

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,10 @@
+"""Client configuration"""
 import pytest
 from src.app import app
 
 @pytest.fixture
 def client():
+    """Client definition"""
     app.config['TESTING'] = True
     with app.test_client() as test_client:
         yield test_client
--- test/test_app.py
+++ test/test_app.py
@@ -1,14 +1,11 @@
-# @pytest.mark.parametrize()
+"""endpoints unit tests"""
 def test_get_taxis(client):
-    # Prueba básica para asegurarse de que la ruta de los taxis está disponible
+    """Testing /taxis route is available"""
     response = client.get('/taxis')
     assert response.status_code == 200
 
 def test_get_taxis_pagination(client):
-    # Prueba para verificar si la paginación funciona correctamente
+    """Testing /taxis pagination is correctly working"""
     response = client.get('/taxis?page=2&per_page=10')
     data = response.get_json()
-    assert len(data) == 10  # Comprueba si devuelve 10 taxis por página
-
-# if __name__ == '__main__':
-#     pytest.main()
+    assert len(data) == 10  # Checks if returns 10 taxis per page
