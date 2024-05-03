7f8c0e27dd941035a9bd9a595416f41ac31fdf38
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-05-03 13:52:47 -0500

    taxi_id added as part of the path and date as query param

--- src/models/trajectory_route.py
+++ src/models/trajectory_route.py
@@ -6,23 +6,22 @@ from src.models.models import Trajectory
 
 trajectory_bp = Blueprint("trajectory_bp", __name__)
 
-@trajectory_bp.route("/trajectories", methods=["GET"])
-def get_trajectories():
+@trajectory_bp.route("/trajectories/<int:taxi_id>", methods=["GET"])
+def get_trajectories(taxi_id):
     """Get list of taxis trajectories"""
-    # Getting taxi_id and date params
-    taxi_id = request.args.get('taxi_id')
+    # Getting date param
     date_str = request.args.get('date')
 
     # Pagination limits
     page = request.args.get('page', default=1, type=int)
     per_page = request.args.get('per_page', default=10, type=int)
 
-    if not taxi_id or not date_str:
-        return jsonify({"error": "Both taxi_id and date parameters are required"}), 400
+    if not date_str:
+        return jsonify({"error": "date parameter is required"}), 400
 
     try:
         date = datetime.strptime(date_str, '%Y-%m-%d').date()
-        date_tomorrow = date + timedelta(days=1) # Adds the quantity of seconds in one
+        date_tomorrow = date + timedelta(days=1) # Adds the quantity of seconds in one day
 
     except ValueError:
         return jsonify({"error": "Invalid date format. Please use YYYY-MM-DD format"}), 400
