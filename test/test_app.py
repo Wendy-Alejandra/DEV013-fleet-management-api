3d5a2f328c622e5b7175e56307db61b348054037
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-05-06 10:59:35 -0500

    C:/Program Files/Git/trajectories/texi_id unit tests added and running 100%

--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,5 @@
 """endpoints unit tests"""
+# taxis endpoint unit tests
 def test_get_taxis(client):
     """Testing /taxis route is available"""
     response = client.get('/taxis')
@@ -14,3 +15,25 @@ def test_get_taxis_pagination(client):
     response = client.get('/taxis?page=2&per_page=10')
     data = response.get_json()
     assert len(data) == 10  # Checks if returns 10 taxis per page
+
+# trajectories endpoint unit tests
+def test_get_trajectories(client):
+    """Testing /trajectories/taxi_id path is available"""
+    response = client.get('/trajectories/6418?date=2008-02-02&page=10&per_page=10')
+    assert response.status_code == 200
+
+def test_get_trajectories_without_date(client):
+    """Testing /trajectories/taxi_id without date"""
+    response = client.get('/trajectories/6418?page=10&per_page=10')
+    assert response.status_code == 400
+
+def test_get_trajectories_invalid_format(client):
+    """Testing /trajectories/taxi_id with invalid date format"""
+    response = client.get('/trajectories/6418?date="2008-02-02"&page=10&per_page=10')
+    assert response.status_code == 400
+
+def test_get_trajectories_pagination(client):
+    """Testing /trajectories/taxi_id pagination is correctly working"""
+    response = client.get("/trajectories/6418?date=2008-02-02&page=10&per_page=10")
+    data = response.get_json()
+    assert len(data) == 10
