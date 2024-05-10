f3f909d7f5cd0fd8a93498ce5b001b386aea6a61
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-05-10 08:49:54 -0500

    Created migrate_data() fixture in conftest.py * To transfer all the data in postgres db to sqlite dbfor testing purposes * Not able Trajectory model to read in the file

--- test/conftest.py
+++ test/conftest.py
@@ -1,29 +1,98 @@
 """Client configuration"""
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from src.app import create_app
-from src.config import config # db
+from src.config import config
+from src.models.models import Taxi, Trajectory
 
 app = create_app()
 
 @pytest.fixture
 def client():
     """Client definition"""
-    testing_confing = config["testing"]
-    app.config.from_object(testing_confing)
-    # Inserts using sqlalchemy api
-    # import taxi model
-    # from sqlalchemy import insert
-    # >>> from sqlalchemy import create_engine
-    # >>> engine = create_engine("sqlite://", echo=True)
-    # Base.metadata.create_all(engine)
-    # stmt = insert(Product).values(name='Printer', price=149.99)
-    # engine.connect().execute(stmt)
     with app.test_client() as test_client:
         yield test_client
+
+@pytest.fixture
+def migrate_data():
+    """Migrate (Create and insert) Postgres db data into sqlite db"""
+    # PostgreSQL db connection Set up
+    postgres_db_uri = config["development"]
+    app.config.from_object(postgres_db_uri)
+    postgres_engine = create_engine(postgres_db_uri)
+    Session = sessionmaker(bind=postgres_engine)
+    postgres_session = Session()
+
+    # trajectory = Trajectory()
+    # Query data from PostgreSQL tables
+    taxis = postgres_session.query(Taxi).limit(5).all()
+    trajectories = postgres_session.query(Trajectory).limit(10).all()
+
+    # Then insert the above data (queried in Postgres) into sqlite db
+    sqlite_db_uri = config["testing"]
+    app.config.from_object(sqlite_db_uri)
+    sqlite_engine = create_engine(sqlite_db_uri)
+    Session = sessionmaker(bind=sqlite_engine)
+    sqlite_session = Session()
+
+    for taxi in taxis:
+        new_taxi = Taxi(
+            id=taxi.id,
+            plate=taxi.plate
+        )
+        sqlite_session.add(new_taxi)
+
+    for Trajectory in trajectories:
+        new_trajectory = Trajectory(
+            taxi_id=Trajectory.taxi_id,
+            date=Trajectory.date,
+            latitude=Trajectory.latitude,
+            longitude=Trajectory.longitude
+        )
+
+    # Commit to save the changes
+    sqlite_session.commit()
+
+    # Close the sessions
+    postgres_session.close()
+    sqlite_session.close()
 
 # @pytest.fixture
 # def client():
 #     """Client definition"""
-#     app.config['TESTING'] = True
+#     # Configure Flask application in testing mode
+#     testing_config = config["testing"]
+#     print(testing_config)
+#     app.config.from_object(testing_config)
+#     # Configure a test client
+#     with app.test_client() as test_client:
+#         yield test_client
+
+# @pytest.fixture
+# def init_database(client):
+#     """Initializing SQLite testing db"""
+#     with app.app_context():
+#         taxi1 = Taxi(id=7249, plate="CNCJ-2997")
+#         taxi2 = Taxi(id=10133,	plate="PAOF-6727")
+#         db.session.add(taxi1)
+#         db.session.add(taxi2)
+#         db.session.commit()
+#         yield
+
+# print(init_database)
+# @pytest.fixture
+# def client():
+#     """Client definition"""
+#     testing_confing = config["testing"]
+#     app.config.from_object(testing_confing)
+#     # Inserts using sqlalchemy api
+#     # import taxi model
+#     # from sqlalchemy import insert
+#     # >>> from sqlalchemy import create_engine
+#     # >>> engine = create_engine("sqlite://", echo=True)
+#     # Base.metadata.create_all(engine)
+#     # stmt = insert(Product).values(name='Printer', price=149.99)
+#     # engine.connect().execute(stmt)
 #     with app.test_client() as test_client:
 #         yield test_client
--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,5 @@
 """endpoints unit tests"""
+import datetime
 # taxis endpoint unit tests
 def test_get_taxis(client):
     """Testing /taxis route is available"""
@@ -40,5 +41,13 @@ def test_get_trajectories_pagination(client):
 
 # def test_get_trajectories_date_str(client):
 #     response = client.get("/trajectories/6418?date=2008-02-02&page=10&per_page=10")
-#     assert response == '2008-02-02'
-    
+#     assert '2008-02-02' in response
+
+# def test_trajectories_data_limit(migrate_data):
+#     expected_value = [{"taxi_id": 6598, "date":	datetime.datetime(2008, 2, 2, 13, 40, 26), "latitude": 116.29308, "longitude": 39.8804, "id": 1},
+#                         {"taxi_id": 6598, "date":	datetime.datetime(2008, 2, 2, 13, 40, 26), "latitude": 116.29308, "longitude": 39.8804, "id": 2},
+#                         {"taxi_id": 6598, "date":	datetime.datetime(2008, 2, 2, 13, 45, 26), "latitude": 116.29308, "longitude": 39.88038, "id": 3},
+#                         {"taxi_id": 6598, "date":	datetime.datetime(2008, 2, 2, 13, 50, 26), "latitude": 116.29296, "longitude": 39.88037, "id": 4},
+#                         {"taxi_id": 6598, "date":	datetime.datetime(2008, 2, 2, 13, 55, 26), "latitude": 116.29301, "longitude": 39.88031, "id": 5},
+#     ]
+#     assert get_trajectories(migrate_data) == expected_value
