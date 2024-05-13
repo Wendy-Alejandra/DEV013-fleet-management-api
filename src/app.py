185acbcd956a3824ffc4be4e922338d1486a1a8d
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-05-13 17:35:25 -0500

    configuring sqlite connection, environment env added, to be changed

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 """Flask application"""
+import os
 from flask import Flask
 from flasgger import Swagger
 from src.config import config
@@ -6,11 +7,11 @@ from src.models.models import db
 from src.models.taxi_route import taxi_bp
 from src.models.trajectory_route import trajectory_bp
 
-def create_app(config):
+def create_app():
     """Create Flask Application"""
     # Instance of Flask class. Argument __name__ is the name of the application’s module or package.
     app = Flask(__name__)
-    app.config.from_object(config) # calling development mode from the dictionary
+    app.config.from_object(config[os.getenv('ENVIRONMENT')]) # calling development mode from the dictionary
 
     # initialize the SQLAlchemy extension class with the application by calling :
     db.init_app(app)
@@ -31,5 +32,5 @@ def create_app(config):
 
 # If the name of the app from main route __main__ then execute our app with the run() cmd
 if __name__ == "__main__":
-    app = create_app(config["development"])
+    app = create_app()
     app.run(debug=True, port=5000) # Debugger activated
--- test/conftest.py
+++ test/conftest.py
@@ -1,96 +1,42 @@
 """Client configuration"""
 import pytest
-# from sqlalchemy import create_engine, text #MetaData
-# from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, text
 from src.app import create_app
 from src.config import config
-# from src.models.models import Taxi, Trajectory
 
-app = create_app(config["testing"])
+app = create_app()
+# engine = None
 
 @pytest.fixture
 def client():
     """Client definition"""
+    print("client")
+    engine = None
+    if not engine:
+        engine = sqlite_connection()
     with app.test_client() as test_client:
         yield test_client
 
+print("otra vez")
 # @pytest.fixture
-# def sqlite_connection():
-#     """Migrate (Create and insert) Postgres db data into sqlite db"""
-#     # PostgreSQL db connection Set up
-#     sqlite_db_uri = config["testing"]
-#     app.config.from_object(sqlite_db_uri)
-#     sqlite_engine = create_engine(sqlite_db_uri)
-#     # metadata = MetaData(bind=sqlite_engine)
-#     # Session = sessionmaker(bind=sqlite_engine)
-#     # sqlite_session = Session()
-
-#     with sqlite_engine.connect() as connection:
-#         result = connection.execute(text("create table taxis (id serial primary key, plate varchar(9) not null);"))
-#         print(result)
-    # Query to create tables into sqlite db
-    # metadata_obj = MetaData()
-    # tables = metadata_obj.create_all(sqlite_engine)
-    # trajectories = Trajectory.create(sqlite_engine)
-
-    # Query to insert data into sqlite db
-
-    # for taxi in taxis:
-    #     new_taxi = Taxi(
-    #         id=taxi.id,
-    #         plate=taxi.plate
-    #     )
-    #     sqlite_session.add(new_taxi)
-
-    # for Trajectory in trajectories:
-    #     new_trajectory = Trajectory(
-    #         taxi_id=Trajectory.taxi_id,
-    #         date=Trajectory.date,
-    #         latitude=Trajectory.latitude,
-    #         longitude=Trajectory.longitude
-    #     )
-
-    # # Commit to save the changes
-    # sqlite_session.commit()
-
-    # # Close the sessions
-    # sqlite_session.close()
-
-# @pytest.fixture
-# def client():
-#     """Client definition"""
-#     # Configure Flask application in testing mode
-#     testing_config = config["testing"]
-#     print(testing_config)
-#     app.config.from_object(testing_config)
-#     # Configure a test client
-#     with app.test_client() as test_client:
-#         yield test_client
-
-# @pytest.fixture
-# def init_database(client):
-#     """Initializing SQLite testing db"""
-#     with app.app_context():
-#         taxi1 = Taxi(id=7249, plate="CNCJ-2997")
-#         taxi2 = Taxi(id=10133,	plate="PAOF-6727")
-#         db.session.add(taxi1)
-#         db.session.add(taxi2)
-#         db.session.commit()
-#         yield
-
-# print(init_database)
-# @pytest.fixture
-# def client():
-#     """Client definition"""
-#     testing_confing = config["testing"]
-#     app.config.from_object(testing_confing)
-#     # Inserts using sqlalchemy api
-#     # import taxi model
-#     # from sqlalchemy import insert
-#     # >>> from sqlalchemy import create_engine
-#     # >>> engine = create_engine("sqlite://", echo=True)
-#     # Base.metadata.create_all(engine)
-#     # stmt = insert(Product).values(name='Printer', price=149.99)
-#     # engine.connect().execute(stmt)
-#     with app.test_client() as test_client:
-#         yield test_client
+def sqlite_connection():
+    """Create SQLite connection and insert data"""
+    print("SQLite connection")
+    # Configure app for testing
+    app.config.from_object(config["testing"])
+    # SQLite db connection set up
+    sqlite_engine = create_engine(config["testing"].SQLALCHEMY_DATABASE_URI)
+    
+    # Queries
+    with sqlite_engine.connect() as connection:
+        # Query to create taxi table in sqlite db
+        taxis = connection.execute(text("create table taxis (id serial primary key, plate varchar(9) not null);"))
+        # Query to insert data into taxi table in sqlite db
+        insert_data = connection.execute(taxis.insert(), [{"id":7249, "plate":"CNCJ-2997"},
+            {"id":10133, "plate":"PAOF-6727"},
+            {"id":6418, "plate":"GHGH-1458"}
+        ])
+        print(insert_data)
+        # Close connection
+        connection.close()
+    return sqlite_engine
